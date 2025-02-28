6b2baf67eeb99e3e949a911ac2bda91e8a3e4b91
engelmeierdaniel <daniel_engelmeier@hotmail.com>
2025-02-28 16:55:30 -0300

    inclusão da classe em um pacote e encapsulamento dos atributos

--- src/Principal.java
+++ src/Principal.java
@@ -1,18 +1,18 @@
+import br.com.alura.screenmatch.modelos.Filme;
 
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
         meuFilme.nome = "O poderoso chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        //meuFilme.anoDeLancamento = 1970;
+        //meuFilme.duracaoEmMinutos = 180;
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8.0);
         meuFilme.avalia(10);
         meuFilme.avalia(5);
 
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
 
     }
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,23 +1,29 @@
+package br.com.alura.screenmatch.modelos;
+
 public class Filme {
 
-    String nome;
+    public String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double somaDasAvaliacoes;
-    int totalDeAvaliacoes;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
     int duracaoEmMinutos;
 
-    void exibeFichaTecnica(){
+    public int getTotalDeAvaliacoes(){
+        return totalDeAvaliacoes;
+    }
+
+    public void exibeFichaTecnica(){
         System.out.println("Nome do filme: " + nome);
         System.out.println("Ano de lançamento: " + anoDeLancamento);
     }
 
-    void avalia(double nota){
+    public void avalia(double nota){
         somaDasAvaliacoes += nota;
         totalDeAvaliacoes++;
     }
 
-    double pegaMedia(){
+    public double pegaMedia(){
         return somaDasAvaliacoes / totalDeAvaliacoes;
     }
 
