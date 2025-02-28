dee0aa55540a9e6420419db4443c9332c20d4fb3
engelmeierdaniel <daniel_engelmeier@hotmail.com>
2025-02-28 15:51:14 -0300

    inclusão dos metodos na classe filme e criação da main

---
+++ src/Principal.java
@@ -0,0 +1,19 @@
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8.0);
+        meuFilme.avalia(10);
+        meuFilme.avalia(5);
+
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.pegaMedia());
+
+    }
+}
